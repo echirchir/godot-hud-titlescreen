31f3c4696859ceabf99965c3afe8a2034a4296d4
Elisha Chirchir <elisha.chirchir@dej-technology.de>
2020-04-04 15:02:33 -0500

    Initial commit

---
+++ scripts/title_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	
+	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
+	
+	for button in $Menu/CenterRow/Buttons.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+		
+func _on_Button_pressed(scene_to_load):
+	get_tree().change_scene(scene_to_load)
+
